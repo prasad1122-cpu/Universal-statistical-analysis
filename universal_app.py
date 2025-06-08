e4bbaa0e6166bc536eeec27b49ff0a270839adcf
prasad1122-cpu <prasaddeshpande9049@gmail.com>
2025-06-08 03:37:18 -0700

    Update universal_app.py

--- universal_app.py
+++ universal_app.py
@@ -73,4 +73,6 @@ def get_chart(filename):
     return send_file(os.path.join(CHART_FOLDER, filename), mimetype='image/png')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
